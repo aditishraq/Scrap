bba0043eb68e4e0b873076a67c6726a12bb859ee
Adit <iamaditishraq@gmail.com>
2021-03-07 15:35:55 +0600

    commit

--- photo_scraper.py
+++ photo_scraper.py
@@ -1,37 +1,77 @@
-import os 
-import json 
-import requests 
-from bs4 import BeautifulSoup
+# For explanation of this code, you can visit this video: https://www.youtube.com/watch?v=8AyKJxBxx0M&t=172s
+import os
+import requests # pip install requests #to sent GET requests
+from bs4 import BeautifulSoup # pip install bs4 #to parse html(getting data out from html, xml or other markup languages)
 
-
-
-
-
-
-GOOGLE_IMAGE = \
+# user can input a search keyword and the count of images required
+# download images from google search image
+Google_Image = \
     'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
 
 # The User-Agent request header contains a characteristic string 
 # that allows the network protocol peers to identify the application type, 
 # operating system, and software version of the requesting software user agent.
 # needed for google search
-usr_agent = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+u_agnt = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    'Accept-Encoding': 'none',
-    'Accept-Language': 'en-US,en;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
     'Connection': 'keep-alive',
-}
+} #write: 'my user agent' in browser to get your browser user agent details
 
-SAVE_FOLDER = 'images'
+Image_Folder = 'Images_1'
 
 def main():
-    if not os.path.exists(SAVE_FOLDER):
-        os.mkdir(SAVE_FOLDER)
+    if not os.path.exists(Image_Folder):
+        os.mkdir(Image_Folder)
     download_images()
-    
+
 def download_images():
-    # ask for user input
-    data = input('What are you looking for? ')
-    n_images = int(input('How many images do you want? '))
+    data = input('Enter your search keyword: ')
+    num_images = int(input('Enter the number of images you want: '))
+    
+    print('Searching Images....')
+    
+    search_url = Google_Image + 'q=' + data #'q=' because its a query
+    
+    # request url, without u_agnt the permission gets denied
+    response = requests.get(search_url, headers=u_agnt)
+    html = response.text #To get actual result i.e. to read the html data in text mode
+    
+    # find all img where class='rg_i Q4LuWd'
+    b_soup = BeautifulSoup(html, 'html.parser') #html.parser is used to parse/extract features from HTML files
+    results = b_soup.findAll('img', {'class': 'rg_i Q4LuWd'})
+    
+    #extract the links of requested number of images with 'data-src' attribute and appended those links to a list 'imagelinks'
+    #allow to continue the loop in case query fails for non-data-src attributes
+    count = 0
+    imagelinks= []
+    for res in results:
+        try:
+            link = res['data-src']
+            imagelinks.append(link)
+            count = count + 1
+            if (count >= num_images):
+                break
+            
+        except KeyError:
+            continue
+    
+    print(f'Found {len(imagelinks)} images')
+    print('Start downloading...')
+
+    for i, imagelink in enumerate(imagelinks):
+        # open each image link and save the file
+        response = requests.get(imagelink)
+        
+        imagename = Image_Folder + '/' + data + str(i+1) + '.jpg'
+        with open(imagename, 'wb') as file:
+            file.write(response.content)
+
+    print('Download Completed!')
+    
+
+if __name__ == '__main__':
+    main()
